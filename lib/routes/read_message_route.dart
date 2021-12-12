b29b487b2ad654c5b4889f354b55d5c014186b1b
Anas El boudali <anel-bou@student.1337.ma>
2021-12-12 17:28:01 +0100

    update

--- lib/routes/read_message_route.dart
+++ lib/routes/read_message_route.dart
@@ -14,42 +14,44 @@ class ReadMessageRoute extends StatelessWidget {
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  IconButton(onPressed: () {}, icon: Image.asset('assets/icons/usa_icon.png'), iconSize: 45.0, padding: EdgeInsets.all(0.0),),
-                  SizedBox(height: 8.0),
-                  SmallButton(onPressed: () {}, title: 'Back', icon: Icons.arrow_left_rounded),
-                  SizedBox(height: 10.0),
-                  Container(
-                    padding: EdgeInsets.all(25.0),
-                    height: 500.0,
-                    width: 500.0,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(20.0),
-                        boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.2), spreadRadius: 0.8, blurRadius: 5, offset: const Offset(8, 8),)]
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Verify your email for winning Article V2', style: TextStyle(fontWeight: FontWeight.w600)),
-                        const Padding(child: Divider(color: Colors.grey), padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0)),
-                        Row(
-                          children: [
-                            ClipRRect(
-                              borderRadius: BorderRadius.circular(30.0),
-                              child: Image.asset('assets/images/blank_profile_picture.jpeg', height: 50.0, width: 50.0,),
-                            ),
-                            SizedBox(width: 15.0),
-                            Expanded(flex: 1, child: Text('from: <noreply@winning-article-v2.firebaseapp.com>', style: TextStyle(fontWeight: FontWeight.w400),))
-                          ],
-                        ),
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(onPressed: () {}, icon: Image.asset('assets/icons/usa_icon.png'), iconSize: 45.0, padding: EdgeInsets.all(0.0),),
+                    SizedBox(height: 8.0),
+                    SmallButton(onPressed: () {}, title: 'Back', icon: Icons.arrow_left_rounded),
+                    SizedBox(height: 10.0),
+                    Container(
+                      padding: EdgeInsets.all(25.0),
+                      height: 500.0,
+                      width: 500.0,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(20.0),
+                          boxShadow: [BoxShadow(color: Colors.grey.withOpacity(0.2), spreadRadius: 0.8, blurRadius: 5, offset: const Offset(8, 8),)]
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Verify your email for winning Article V2', style: TextStyle(fontWeight: FontWeight.w600)),
+                          const Padding(child: Divider(color: Colors.grey), padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0)),
+                          Row(
+                            children: [
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(30.0),
+                                child: Image.asset('assets/images/blank_profile_picture.jpeg', height: 50.0, width: 50.0,),
+                              ),
+                              SizedBox(width: 15.0),
+                              Expanded(flex: 1, child: Text('from: <noreply@winning-article-v2.firebaseapp.com>', style: TextStyle(fontWeight: FontWeight.w400),))
+                            ],
+                          ),
 
-                      ],
-                    ),
-                  )
-                ],
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
--- lib/routes/slash_page.dart
+++ lib/routes/slash_page.dart
@@ -12,47 +12,53 @@ class SlashPage extends StatelessWidget {
       body: Stack(
         children: [
           Image.asset('assets/icons/min_corner_circles.png', width: kScreenWidth * 0.5, fit: BoxFit.fitWidth),
+          // SafeArea(
+          //     child: IconButton(onPressed: () {}, icon: Image.asset('assets/icons/usa_icon.png'), iconSize: 45.0, padding: const EdgeInsets.all(15.0),),
+          // ),
           SafeArea(
-              child: Container(width: 400.0, child: IconButton(onPressed: () {}, icon: Image.asset('assets/icons/usa_icon.png'), iconSize: 45.0, padding: const EdgeInsets.all(15.0),)),
-          ),
-          SafeArea(
-            child: Center(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(height: 50.0,),
-                      Center(child: Image.asset('assets/icons/message_sent.png', height: kScreenHeight * 0.25,)),
-                      const SizedBox(height: 20.0,),
-                      const Center(child: Text('What is Lorem Ipsum ?', style: TextStyle(fontSize: 20.0))),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 25.0),
-                        child: Center(
-                          child: Container(
-                            width: 400.0,
-                            height: 110.0,
-                            child: Text(text, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w200), textAlign: TextAlign.center,)
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+              child: Column(
+                // mainAxisSize: MainAxisSize.max,
+                children: [
+                  IconButton(onPressed: () {}, icon: Image.asset('assets/icons/usa_icon.png'), iconSize: 45.0, padding: const EdgeInsets.all(15.0),),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        // mainAxisAlignment: MainAxisAlignment.center,
+                        // crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 50.0,),
+                          Center(child: Image.asset('assets/icons/message_sent.png', height: kScreenHeight * 0.25,)),
+                          const SizedBox(height: 20.0,),
+                          const Center(child: Text('What is Lorem Ipsum ?', style: TextStyle(fontSize: 20.0))),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 25.0),
+                            child: Center(
+                              child: Container(
+                                width: 400.0,
+                                height: 110.0,
+                                child: Text(text, style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w200), textAlign: TextAlign.center,)
+                              ),
+                            ),
                           ),
-                        ),
+                          Container(
+                            width: 400.0,
+                            child: RoundedButton(
+                              icon: Icons.play_arrow_outlined,
+                              title: 'Get started',
+                              onPressed: () {
+                                // Navigator.pop(context);
+                                Navigator.pushNamed(context, '/read_message_route');
+                              },
+                            ),
+                          ),
+                        ],
                       ),
-                      // const SizedBox(height: 30.0,),
-                      Container(
-                        width: 400.0,
-                        child: RoundedButton(
-                          icon: Icons.play_arrow_outlined,
-                          title: 'Get started',
-                          onPressed: () {
-                            // Navigator.pop(context);
-                            Navigator.pushNamed(context, '/read_message_route');
-                          },
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
             ),
           )
@@ -61,3 +67,4 @@ class SlashPage extends StatelessWidget {
     );
   }
 }
+
